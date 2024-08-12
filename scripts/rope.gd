f2d2e083e7a4bbebc01b5bf953622fa3e8bb7288
Cosillow <72038409+Cosillow@users.noreply.github.com>
2024-08-12 17:44:43 -0400

    Stop tracking files that are now in .gitignore and rope is imroved to use parent/child; has problems switching parents after startup...

--- scripts/rope.gd
+++ scripts/rope.gd
@@ -1,129 +1,133 @@
+class_name Rope
 extends Node2D
+
+# TODO: CURRENTLY... the _parent_cache must be of Node2d... otherwise it crashes
+#		idk what the behavior should be for that, probably just fall to gravity?
+#		also... I have zero error checking if there is no _parent_cache
+# TODO: boolean exports for the rope just affecting the _parent_cache/child velocity, not just setting position
+# TODO: ropeSize is not an exact science, because of the constraint size
+#		probably change the constraint size to work for the ropeSize on rope-set
+# TODO: _notification doesn't work all the time for reparenting... I've found it is because
+#		(in some way) the position of the rope is not always 0 when it swaps parents. Should I
+#		be using global position some places instead?
 
 @export var ropeLength:float = 30 :
 	set(length):
 		ropeLength = length
-		call_deferred("init")
+		call_deferred("_ready")
 ## constraint iterations (a ratio more iterations will be applied if rope exceeds length)
 @export var tightness: int = 100 
 ## distance between points (must be < ropeLength)
 @export_range(1, 9999999, 1, "or_greater") var constrain: float = 1 :
 	set(val):
 		constrain =  max(1, min(val, ropeLength - 0.1))
-		call_deferred("init")
+		call_deferred("_ready")
 @export var gravityAdjustment: float = 0
-@export_range(0.1, 0.9, .1) var dampening: float = 0.9
+@export_range(0.1, 0.9, .01) var dampening: float = 0.9
 @export var endPin: bool = true
 @export var color: Color = Color(0.648, 0.389, 0.056)
-@export var attatchedTo: Node2D
-@export var endAttatchment: Node2D
 @export_range(1, 9999999, 1, "or_greater") var width: float = 2
 @onready var prGravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")/50
 var gravity: Vector2 :
 	get:
 		return Vector2(0, prGravity + gravityAdjustment)
-var pointCount: int
-var pos: PackedVector2Array
-var posPrev: PackedVector2Array
+var _point_count: int
+var _pos: PackedVector2Array
+var _pos_prev: PackedVector2Array
+
+var finalPosition: PackedVector2Array :
+	get:
+		return _pos * Transform2D().translated(_parent_cache.position)
+var _parent_cache: Node2D = null
 
 func _ready()->void:
-	# process_physics_priority is set to be after attatchedTo: Node2d
-	# TODO: maybe... `attatchedTo` should just be the parent's position?
-	init()
-
-func init():
-	pointCount = int(ceil(ropeLength / constrain))
-	if attatchedTo:
-		position = attatchedTo.position
-	resize_arrays()
-	init_position()
-
-func resize_arrays():
-	pos.resize(pointCount)
-	posPrev.resize(pointCount)
-
-func init_position()->void:
-	for i in range(pointCount):
-		pos[i] = position + Vector2(0, constrain *i)
-		posPrev[i] = position + Vector2(0, constrain *i)
+	_point_count = int(ceil(ropeLength / constrain))
+	
+	# resize arrays
+	_pos.resize(_point_count)
+	_pos_prev.resize(_point_count)
+	
+	# init position
+	print(self.name, " init position: %v" % [position])
 	position = Vector2.ZERO
+	for i in range(_point_count):
+		_pos[i] = position + Vector2(0, constrain *i)
+		_pos_prev[i] = position + Vector2(0, constrain *i)
 
-#func _unhandled_input(event:InputEvent)->void:
-	#if event is InputEventMouseMotion:
-		#if Input.is_action_pressed("right_click"):	#Move start point
-			#set_last(get_global_mouse_position())
-	#elif event is InputEventMouseButton && event.is_pressed():
-		#if event.button_index == 1:
-			#set_start(get_global_mouse_position())
-		#elif event.button_index == 2:
-			#set_last(get_global_mouse_position())
-
+func _notification(what):
+	match what:
+		NOTIFICATION_PARENTED:
+			var p = get_parent()
+			if !(p is Node2D):
+				_parent_cache = null
+				return
+				
+			_parent_cache = p
+			set_deferred("position", Vector2.ZERO)
+			call_deferred("set_start", _parent_cache.position)
+			print("PARENTED:: position: %v, parent pos: %v" % [position, _parent_cache.position])
+		NOTIFICATION_UNPARENTED:
+			print("UNPARENTED:: position: %v" % [position])
+			_parent_cache = null
 
 func _physics_process(delta)->void:
-	update_points(delta)
+	if not _parent_cache: return
+	_update_points(delta)
 	
 	# tighten rope more if it exceeds ropeLength
-	var distSq = pos[0].distance_squared_to(pos[pointCount-1])
+	var distSq = _pos[0].distance_squared_to(_pos[_point_count-1])
 	var possibleError = distSq / pow(ropeLength,2)
 	var iterations = max(tightness,int(possibleError * tightness))
 	for i in iterations:
-		update_constrain()
+		_update_constrain()
 	
-	if attatchedTo:
-		#call_deferred("set_start", attatchedTo.position)
-		set_start(attatchedTo.position)
-	if endAttatchment:
-		#call_deferred("attatch_last", endAttatchment)
-		attatch_last(endAttatchment)
+	set_start(_parent_cache.position)
 	for c in get_children():
 		if c is Node2D:
-			c.position = pos[pointCount-1]
-	
+			c.position = _pos[_point_count-1] - _parent_cache.position
 	queue_redraw()
-	
+
 func set_start(p:Vector2)->void:
-	pos[0] = p
-	posPrev[0] = p
+	_pos[0] = p
+	_pos_prev[0] = p
 
-func attatch_last(n: Node2D)->void:
-	n.position = pos[pointCount-1]
+func _update_points(delta)->void:
+	for i in range (_point_count):
+		# TODO: may need to adjust this if AFFECTING _parent_cache/child not just changing _pos
+		if (i!=0) || (i==0 && !_parent_cache):
+			var velocity = (_pos[i] -_pos_prev[i]) * dampening
+			_pos_prev[i] = _pos[i]
+			_pos[i] += velocity + (gravity * delta)
 
-func update_points(delta)->void:
-	for i in range (pointCount):
-		# not first and last || first if not pinned || last if not pinned
-		if (i!=0 && i!=pointCount-1) || (i==0 && !attatchedTo) || (i==pointCount-1 && !endPin):
-			var velocity = (pos[i] -posPrev[i]) * dampening
-			posPrev[i] = pos[i]
-			pos[i] += velocity + (gravity * delta)
-
-func update_constrain()->void:
-	for i in range(pointCount):
-		if i == pointCount-1:
+func _update_constrain()->void:
+	for i in range(_point_count):
+		if i == _point_count-1:
 			return
-		var distance = pos[i].distance_to(pos[i+1])
+		var distance = _pos[i].distance_to(_pos[i+1])
 		var difference = constrain - distance
 		var percent = difference / distance
-		var vec2 = pos[i+1] - pos[i]
+		var vec2 = _pos[i+1] - _pos[i]
 		
 		# if first point
 		if i == 0:
-			if attatchedTo:
-				pos[i+1] += vec2 * percent
+			if _parent_cache:
+				_pos[i+1] += vec2 * percent
 			else:
-				pos[i] -= vec2 * (percent/2)
-				pos[i+1] += vec2 * (percent/2)
+				_pos[i] -= vec2 * (percent/2)
+				_pos[i+1] += vec2 * (percent/2)
 		# if last point, skip because no more points after it
-		elif i == pointCount-1:
+		elif i == _point_count-1:
 			pass
 		# all the rest
 		else:
-			if i+1 == pointCount-1 && endPin:
-				pos[i] -= vec2 * percent
+			if i+1 == _point_count-1 && endPin:
+				_pos[i] -= vec2 * percent
 			else:
-				pos[i] -= vec2 * (percent/2)
-				pos[i+1] += vec2 * (percent/2)
+				_pos[i] -= vec2 * (percent/2)
+				_pos[i+1] += vec2 * (percent/2)
 
 func _draw() -> void:
-	draw_polyline(pos, color, float(width))
-	for p in pos:
+	var rope := finalPosition
+	draw_polyline(rope, color, float(width))
+	for p in rope:
 		draw_circle(p, width/1.5, "pink")
