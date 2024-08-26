ce4d918db0d3636bb36fc37610fce72ca1f41792
Cosillow <72038409+Cosillow@users.noreply.github.com>
2024-08-25 23:28:07 -0400

    changed controller and dragging collision

--- scripts/space_controller.gd
+++ scripts/space_controller.gd
@@ -16,18 +16,5 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	for i in state.get_contact_count():
 		if state.get_contact_collider_object(i) == attached_body:
 			var normal = state.get_contact_local_normal(i)
-			#rope_sling.apply_endpoint_impulse(-normal* 30)
-			rope_sling.apply_endpoint_impulse(bounce_ratio * (-normal * state.get_contact_local_velocity_at_position(i).length()))
-			
-			#rope_sling.apply_endpoint_impulse(state.get_contact_local_velocity_at_position(i).bounce(normal))
-
-func _on_attached_collision():
-	pass
-
-#func _on_body_entered(body: Node) -> void:
-	#print(body, " ... ", linear_velocity)
-	#rope_sling.apply_endpoint_impulse(Vector2(1,-1)*linear_velocity)
-
-
-#func _on_body_exited(body: Node) -> void:
-	#rope_sling.apply_endpoint_impulse(-linear_velocity)
+			var relative_velocity = state.get_contact_collider_velocity_at_position(i) - state.get_contact_local_velocity_at_position(i)
+			rope_sling.apply_endpoint_impulse(bounce_ratio * (-normal * relative_velocity.length()))
