f3004f2094f5ab555fe8ac5a94d6142560a76657
Cosillow <72038409+Cosillow@users.noreply.github.com>
2024-09-14 01:27:41 -0400

    world boarder cam

--- scripts/camera_world.gd
+++ scripts/camera_world.gd
@@ -6,16 +6,20 @@ extends Node2D
 
 func _ready() -> void:
 	var center := bordered_cam.get_screen_center_position()
-	var camera_size := get_viewport_rect().size * bordered_cam.zoom
-	var camera_rect := Rect2(center - camera_size / 2, camera_size)
-	var x_from_center := camera_rect.size.x / 2
-	var y_from_center := camera_rect.size.y / 2
-	var positions := [Vector2.UP, -y_from_center, Vector2.DOWN, y_from_center, Vector2.LEFT, -x_from_center, Vector2.RIGHT, x_from_center]
+	bordered_cam.custom_viewport
 	
-	for i in 4:
+	var camera_size := get_viewport().get_visible_rect().size / bordered_cam.zoom
+	var camera_rect := Rect2(center - camera_size / 2, camera_size)
+	var x_from_center := -camera_rect.size.x / 2
+	var y_from_center := -camera_rect.size.y / 2
+	print(camera_size)
+	var positions := [[Vector2.DOWN, y_from_center], [Vector2.UP, y_from_center], [Vector2.RIGHT, x_from_center], [Vector2.LEFT, x_from_center]]
+	
+	for i in range(len(positions)):
 		var shape := CollisionShape2D.new()
 		var boundary := WorldBoundaryShape2D.new()
-		boundary.normal = positions[i]
-		boundary.distance = positions[i+1]
-		shape.shape = boundary
 		world_boundry.add_child(shape)
+		boundary.normal = positions[i][0]
+		#boundary.distance = positions[i][1]
+		shape.position += boundary.normal * positions[i][1]
+		shape.shape = boundary
